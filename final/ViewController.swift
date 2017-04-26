90257af0a0614233e7b610315879b053a1daff4a
procrastination <hengcui@usc.edu>
2017-04-26 04:01:11 -0700

    Initial Commit

---
+++ final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  final
+//
+//  Created by heng cui on 2017/4/26.
+//  Copyright © 2017年 heng cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
